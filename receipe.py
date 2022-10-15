1a6dc233ad1f376877a99fcd79ee730c32b0ea43
Anandprakashh <anandprakash4488@gmail.com>
2022-10-15 21:45:19 +0100

    added new files

---
+++ receipe.py
@@ -0,0 +1,7 @@
+Food = input("Enter your type of food: ")
+Plant = input("Enter the type of plant you like: ")
+Cook = input("Enter the method for your reciepe: ")
+Burn = input("Enter a word to describe burned food: ")
+Item = input("Enter the household item: ")
+print()
+print(Cook, Food, "with", Burn, Plant, "on a bed of", Item)
