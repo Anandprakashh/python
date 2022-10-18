44427942393db5bf7f99716faee77a1a745ed13d
Anandprakashh <anandprakash4488@gmail.com>
2022-10-18 21:16:32 +0100

    added a new file

---
+++ adventure.py
@@ -0,0 +1,9 @@
+print("""Hi, Welcome to the adventure!
+I am going to ask you some questions and then create an epic story""")
+name = input("What is your name: ")
+like = input("What do you like the most: ")
+hate = input("What do you hate the most: ")
+live = input("Where do you live: ")
+power = input("What is your super power: ")
+print()
+print("Hello", name,"your ability to", power, "is good for make your life better and it will keep", hate, "away. Have a ride on your", live, "streets and do", like, "to make you feel happy.")
