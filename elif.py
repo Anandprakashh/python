4e58c46e236aea76ab1ebf02cae09cf5929c0291
Anandprakashh <anandprakash4488@gmail.com>
2022-10-19 10:37:53 +0100

    added a marvel's character

---
+++ elif.py
@@ -0,0 +1,12 @@
+flash = input("Do you like to run?: ")
+Thor = input("Do you like thunder?: ")
+hulk = input("Do you like to smash?: ")
+
+if flash == "yes":
+  print("Then you are a flash.")
+elif Thor == "yes":
+  print("Then you are a Thor")
+elif hulk == "yes":
+  print("Then you are a hulk")
+else:
+  print("sorry, it seems you don't like superpowers.")
---
+++ ifelse.py
@@ -0,0 +1,5 @@
+myName = input("What's your name?: ")
+if myName == "David":
+  print("Welcome Dude!")
+else:
+  print("sorry, you can't access")
