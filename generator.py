a77a1b780c178ba69afa737980f490691c8e18f3
Anandprakashh <anandprakash4488@gmail.com>
2022-10-21 14:07:26 +0100

    added a combination file

---
+++ generator.py
@@ -0,0 +1,18 @@
+print("Affirmations Generator")
+print()
+name = input("Please enter your name: ")
+date = input("Please enter the current date: ")
+things = input("What is your favorite colour: ")
+if things == "green" or "black":
+  print("Oh! thats a cool colour")
+  if things == "blue":
+    print("Oh! fantastic choice")
+else:
+  print("oh! wonderful")
+food = input("What is your favourite food: ")
+if food == "rice" or "Rice":
+  print("It has more carbohydrates")
+  if food == "burger":
+    print("It's not a healthy food")
+else:
+  print("Oh! good choice.")
---
+++ nestedif.py
@@ -0,0 +1,17 @@
+tvShow = input("What is your favorite tv show? ")
+if tvShow == "the vikings":
+  print("Oh!, cool")
+  faveCharacter = input("Who is your favorite character? ")
+  if faveCharacter == "ragnor":
+    print("Sounds Great!")
+  else:
+    print("Nah, Ragnor the greatest")
+elif tvShow == "the witcher":
+  print("Aww, good series")
+  favcharacter = input("Who is your fav character: ?")
+  if favcharacter == "patricia":
+    print("She is cool")
+  else:
+    print("Not bad")
+else:
+  print("Yeah, that's cool and all…")
