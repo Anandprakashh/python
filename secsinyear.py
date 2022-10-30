6fe800b8bf737a9fe2945f7e78fb651be9c6b184
Anandprakashh <anandprakash4488@gmail.com>
2022-10-30 17:03:17 +0000

    added new codes

---
+++ secsinyear.py
@@ -0,0 +1,7 @@
+days = int(input("Enter the number of days: "))
+if days == 365:
+  print(days * 24 * 60 * 60)
+elif days == 366:
+  print(days * 24 * 60 * 60)
+else:
+  print("Not the correct answer")
---
+++ setsdiff.py
@@ -0,0 +1,4 @@
+dev_team = {"John", "Chris", "Leena"}
+test_team = {"Ivana", "Rachel", "Chris"}
+dev_team_diff = dev_team.difference(test_team)
+print(dev_team_diff)
